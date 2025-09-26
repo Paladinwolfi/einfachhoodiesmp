4fd806c3e905def5dfc1a9f52f2fd928e19e57a0
Paladinwolfi <paladin@paladinwolfi.ch>
2025-09-26 10:32:13 +0200

    Db connection renaming

--- HoodieStatsApi/Program.cs
+++ HoodieStatsApi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
+        builder.Configuration.GetConnectionString("TestDbConnection"),
         new MySqlServerVersion(new Version(8, 0, 36))
     )
 );
